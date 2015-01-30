2f4f0285a4780f5db6405f128270445576e92444
steve <steven.v.miller@gmail.com>
2015-01-30 17:09:41 -0500

    Update 1-load.R

--- 1-load.R
+++ 1-load.R
@@ -5,5 +5,6 @@ library(lattice)
 library(lme4)
 library(Zelig)
 
+# Make sure you know where these are. Be mindful of differences in your versions.
 LB03 <- read.dta("~/Dropbox/data/latinobarometro/dta/latinobarometro2003_eng.dta", convert.factors = FALSE)
 LB06 <- read.dta("~/Dropbox/data/latinobarometro/dta/Latinobarometro_2006_eng.dta", convert.factors = FALSE)
