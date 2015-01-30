1d870d78ba695f8e00984484fab975607ca14c9c
steve <steven.v.miller@gmail.com>
2015-01-30 11:41:36 -0500

    Create 0-source.R

---
+++ 0-source.R
@@ -0,0 +1,11 @@
+#
+# Clean Latinobarometro (2003, 2006).
+# -----------------------------------------------------
+# Steven V. Miller
+# Date: 25 January 2015
+
+setwd("~/Dropbox/projects/lb-sacrifice-personal-interests") # Call it whatever you want here.
+
+source("1-load.R")
+source("2-clean.R")
+source("3-analysis.R")
